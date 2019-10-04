d951e491c2bc107d1db29e6487573a40a156eab5
ElviraHoward <elviravalo@gmail.com>
2019-10-04 22:12:56 +0400

    vk auth

--- app/src/main/java/com/elvira/vkhelper/MainActivity.java
+++ app/src/main/java/com/elvira/vkhelper/MainActivity.java
@@ -1,13 +1,46 @@
 package com.elvira.vkhelper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] vkscope = new String[]{VKScope.WALL, VKScope.FRIENDS, VKScope.PHOTOS};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VKSdk.login(this, vkscope);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // success auth
+                Toast.makeText(getApplicationContext(), "Success auth", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(VKError error) {
+                // fail auth
+                Toast.makeText(getApplicationContext(), "Failed auth", Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 }
